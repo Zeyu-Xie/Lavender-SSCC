94b82044a92c46c4470e1ead199bbc81d9ec80b0
Acan <110075452+Zeyu-Xie@users.noreply.github.com>
2025-04-24 23:46:39 +0200

    Updated a version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Lavender-SSCC",
-    version="0.0.4",
+    version="0.0.5",
     description="A pip package to calculate similarity in shape among Chinese characters.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
